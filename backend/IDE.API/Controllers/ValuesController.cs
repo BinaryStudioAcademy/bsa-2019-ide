13cc76b9d470ea59350ba10ae1b736f9de703ff9
Nikita Potapenko <dev.potapy4@hotmail.com>
2019-07-30 16:26:57 +0300

    Remove api prefix

--- backend/IDE.API/Controllers/ValuesController.cs
+++ backend/IDE.API/Controllers/ValuesController.cs
@@ -6,37 +6,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IDE.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET /values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET /values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST /values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT /values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE /values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
