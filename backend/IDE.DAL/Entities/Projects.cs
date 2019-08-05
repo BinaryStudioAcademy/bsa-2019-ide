aee17d74a8230c6213a45fd9d823708faa4b3aa2
Yevhenii Moroziuk <kemd99@gmail.com>
2019-08-05 15:19:07 +0300

    Create Projects.cs

---
+++ backend/IDE.DAL/Entities/Projects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.DAL.Entities
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public Languages Language { get; set; }
+        public ProjectTypes ProjectType { get; set; }
+        public CompilerTypes CompilerType { get; set; }
+        public int MyProperty { get; set; }
+        public string ProjectLink { get; set; }
+        public int AuthorId { get; set; }
+        public string GitUrl{ get; set; }
+        public string GitCredentials { get; set; }
+        public int CountOfSaveBuilds { get; set; }
+        public int CountOfBuildAttempts { get; set; }
+    }
+}
