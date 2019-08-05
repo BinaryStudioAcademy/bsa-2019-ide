86c42ea2e648488efb376ae1f819e725857b449c
Tania <tatiana.gutiy@gmail.com>
2019-08-05 15:10:19 +0300

    Created User.cs

--- backend/IDE.DAL/Entities/User.cs
+++ backend/IDE.DAL/Entities/User.cs
@@ -6,5 +6,16 @@ namespace IDE.DAL.Entities
 {
     public class User
     {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+        string PasswordHash { get; set; }
+        string PasswordSalt { get; set; }
+        DateTime Birthday { get; set; }
+        DateTime RegisteredAt { get; set; }
+        DateTime LastActive { get; set; }
+        string LogoUrl { get; set; }
+        string GitHubUrl { get; set; }
     }
 }
