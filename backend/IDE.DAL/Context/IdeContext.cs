45c1eb9f619651be2a64dafd49842b2534f6734a
_ext Kotsabiuk, Mykola <mykola.kotsabiuk.ext@dieboldnixdorf.com>
2019-08-05 21:27:47 +0300

    Added EF Core, context, model builder extention and setuped connection string

---
+++ backend/IDE.DAL/Context/IdeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IDE.DAL.Context
+{
+    public sealed class IdeContext : DbContext
+    {
+        public IdeContext(DbContextOptions<IdeContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Configure();
+
+            modelBuilder.Seed();
+        }
+    }
+}
---
+++ backend/IDE.DAL/Context/ModelBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.DAL.Context
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Configure(this ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
