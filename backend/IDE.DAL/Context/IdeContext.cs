0186c94956d4b347a9ef695bf3a37089ef7c46f5
_ext Kotsabiuk, Mykola <mykola.kotsabiuk.ext@dieboldnixdorf.com>
2019-08-05 21:32:47 +0300

    Addded DbSet entities

--- backend/IDE.DAL/Context/IdeContext.cs
+++ backend/IDE.DAL/Context/IdeContext.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IDE.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace IDE.DAL.Context
 {
     public sealed class IdeContext : DbContext
     {
         public IdeContext(DbContextOptions<IdeContext> options) : base(options) { }
+
+        public DbSet<Build> Builds { get; private set; }
+        public DbSet<LastSuccessfulBuild> LastSuccessfulBuilds { get; private set; }
+        public DbSet<Project> Projects { get; private set; }
+        public DbSet<User> Users { get; private set; }
+        public DbSet<ProjectMember> ProjectMembers { get; private set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
