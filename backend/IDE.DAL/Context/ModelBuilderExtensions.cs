2162d993e75549bea1ad694fa92821c90dbe87d8
Yevhenii Moroziuk <kemd99@gmail.com>
2019-08-05 22:44:23 +0300

    Update ModelBuilderExtensions.cs

--- backend/IDE.DAL/Context/ModelBuilderExtensions.cs
+++ backend/IDE.DAL/Context/ModelBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IDE.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +11,27 @@ namespace IDE.DAL.Context
         public static void Configure(this ModelBuilder modelBuilder)
         {
             // Add Fluent API configuration here
+            modelBuilder.Entity<ProjectMember>()
+               .HasKey(pm => new { pm.ProjectId, pm.UserId });
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.Project)
+                .WithMany(p => p.ProjectMembers)
+                .HasForeignKey(pm => pm.ProjectId);
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.User)
+                .WithMany(u => u.ProjectMembers)
+                .HasForeignKey(pm => pm.UserId);
+
+            modelBuilder.Entity<Build>()
+               .HasKey(b => new { b.ProjectId, b.UserId });
+            modelBuilder.Entity<Build>()
+                .HasOne(b => b.Project)
+                .WithMany(p => p.Builds)
+                .HasForeignKey(pm => pm.ProjectId);
+            modelBuilder.Entity<Build>()
+                .HasOne(pm => pm.User)
+                .WithMany(u => u.Builds)
+                .HasForeignKey(pm => pm.UserId);
         }
 
         public static void Seed(this ModelBuilder modelBuilder)
