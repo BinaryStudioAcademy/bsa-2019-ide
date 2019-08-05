5420b04c3251894e2d2317d5f95bd632ceae1598
Tania <tatiana.gutiy@gmail.com>
2019-08-05 15:38:23 +0300

    Added ProjectMembers.cs

---
+++ backend/IDE.DAL/Entities/ProjectMembers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.DAL.Entities
+{
+    public class ProjectMembers
+    {
+        int Id { get; set; }
+        int ProjectId { get; set; }
+        int UserId { get; set; }
+        UserAccesses UserAcces { get; set; }
+    }
+}
