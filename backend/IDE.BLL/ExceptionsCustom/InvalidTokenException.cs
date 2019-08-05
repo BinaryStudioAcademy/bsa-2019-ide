7e6e1c4f78ae98d7b6438ee43e7d3676afaee3c9
rshul <romander@ukr.net>
2019-08-05 21:29:40 +0300

    added JwtFactory class

---
+++ backend/IDE.BLL/ExceptionsCustom/InvalidTokenException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.BLL.ExceptionsCustom
+{
+    public class InvalidTokenException : Exception
+    {
+        public InvalidTokenException(string tokenName) : base($"invalid token {tokenName}")
+        {
+
+        }
+    }
+}
