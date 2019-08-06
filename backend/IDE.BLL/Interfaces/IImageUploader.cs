3aa88e45d8e6a696f6c81f121b92323fd3ef00b7
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2019-08-06 19:19:25 +0300

    Added IImageUploader

---
+++ backend/IDE.BLL/Interfaces/IImageUploader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.BLL.Interfaces
+{
+    interface IImageUploader
+    {
+        string UploadFromBase64(string base64);
+        string UploadFromByteArray(byte[] byteArray);
+        string UploadFromUrl(string url);
+    }
+}
