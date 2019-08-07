84ddce2532d3057fa8af578a92530359c0c0f3fc
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2019-08-07 10:22:49 +0300

    Added start implementation of ImgurUploaderService

--- backend/IDE.BLL/Interfaces/IImageUploader.cs
+++ backend/IDE.BLL/Interfaces/IImageUploader.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IDE.BLL.Interfaces
+﻿namespace IDE.BLL.Interfaces
 {
     interface IImageUploader
     {
---
+++ backend/IDE.BLL/Services/ImgurUploaderService.cs
@@ -0,0 +1,73 @@
+﻿using IDE.BLL.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDE.BLL.Services
+{
+    class ImgurUploaderService : IImageUploader
+    {
+        private readonly string _imgurClientId;
+        private readonly string _imgurClientSecret;
+        private readonly string _imgurApiUrl;
+        // private readonly IConfiguration _configuration;
+        private readonly HttpClient _client = new HttpClient();
+
+
+        public ImgurUploaderService(IConfiguration configuration)
+        {
+            // _configuration = configuration;
+            _imgurClientId = configuration["BsaIdeImgurClientId"];
+            _imgurClientSecret = configuration["BsaIdeImgurClientSecret"];
+            _imgurApiUrl = configuration.GetSection("ImgurApiUrl").Value;
+        }
+
+        public async Task<string> UploadFromBase64Async(string imageBase64, string imageTitle)
+        {
+            var body = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string, string>("image", imageBase64),
+                new KeyValuePair<string, string>("title", imageTitle),
+                new KeyValuePair<string, string>("type", "base64")
+            });
+
+            // headers??
+
+            var imageUploadResult = await _client.PostAsync($"{_imgurApiUrl}/image", body);
+
+
+
+            return "";
+        }
+
+        public string UploadFromByteArray(byte[] byteArray)
+        {
+            return "";
+        }
+
+        public string UploadFromUrl(string url)
+        {
+            return "";
+        }
+
+        public string Upload(string imageAsBase64String)
+        {
+            string result = null;
+            using (var webClient = new WebClient())
+            {
+                var values = new NameValueCollection
+                {
+                    { "key", _imgurClientId },
+                    { "image", imageAsBase64String },
+                    { "type", "base64" },
+                };
+                byte[] response = webClient.UploadValues("http://api.imgur.com/2/upload.xml", "POST", values);
+            }
+            return result;
+        }
+    }
+}
