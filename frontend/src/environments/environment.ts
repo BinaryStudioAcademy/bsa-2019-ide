bc95dffc0cff5ec0054e22416a1a82dec4de42dc
rshul <romander@ukr.net>
2019-08-07 11:42:00 +0300

    httpclient service  wrapper done

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44352'
+  apiUrl: 'https://localhost:44352/'
 };
-
+/*vscode - apiUrl:'https://localhost:5001'*/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
