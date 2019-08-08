432ce1f67ab51e3b366f9f5b8d9504085c976cb3
rshul <romander@ukr.net>
2019-08-08 17:10:36 +0300

    workspace created

---
+++ frontend/src/app/modules/workspace/editor-section/editor-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-section',
+  templateUrl: './editor-section.component.html',
+  styleUrls: ['./editor-section.component.sass']
+})
+export class EditorSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
