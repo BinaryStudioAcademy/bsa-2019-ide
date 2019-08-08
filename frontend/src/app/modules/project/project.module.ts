8a6d97a7bbf5822f41075642c23178577272e006
Anatolii Ivanov <anatolii.0700@gmail.com>
2019-08-08 11:11:49 +0300

    Move component of project creation to project module

--- frontend/src/app/modules/project/project.module.ts
+++ frontend/src/app/modules/project/project.module.ts
@@ -4,9 +4,15 @@ import { ProjectRootComponent } from './components/project-root/project-root.com
 import { ProjectSettingsComponent } from './components/project-settings/project-settings.component';
 import { ProjectDetailsComponent } from './components/project-details/project-details.component';
 import { ProjectRoutingModule } from './project-routing.module';
+import { CreateProjectComponent } from './components/create-project/create-project.component';
 
 @NgModule({
-  declarations: [ProjectRootComponent, ProjectSettingsComponent, ProjectDetailsComponent],
+  declarations: [
+    ProjectRootComponent,
+    ProjectSettingsComponent,
+    ProjectDetailsComponent,
+    CreateProjectComponent
+  ],
   imports: [
     CommonModule,
     ProjectRoutingModule
