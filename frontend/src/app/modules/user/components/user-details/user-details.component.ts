99489584369889945324616d1b41b2633f4970a7
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-08-12 20:37:10 +0300

    add opportunities expand avatar

--- frontend/src/app/modules/user/components/user-details/user-details.component.ts
+++ frontend/src/app/modules/user/components/user-details/user-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service/user.service';
+import { UserDetailsDTO } from 'src/app/models/DTO/User/userDetailsDTO';
 
 @Component({
   selector: 'app-user-details',
@@ -9,10 +10,18 @@ import { UserService } from 'src/app/services/user.service/user.service';
     ]
 })
 export class UserDetailsComponent implements OnInit {
-
+  private user : UserDetailsDTO;
+  private isImageExpended: boolean = false;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+      this.user = this.userService.getUserDetails();
   }
-
+  expandImage(imageUrl : string){
+      this.isImageExpended = true;
+      console.log(imageUrl);
+  }
+  colapseImage(){
+      this.isImageExpended = false;
+  }
 }
