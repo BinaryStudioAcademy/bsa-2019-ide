17e66bd24dd2426c9abcbc793062cc97ceb06035
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-08-12 22:45:58 +0300

    added dialog

--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -4,12 +4,14 @@ import { UserRootComponent } from './components/user-root/user-root.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { UserSettingsComponent } from './components/user-settings/user-settings.component';
 import { UserRoutingModule } from './user-routing.module';
+import {DialogModule} from 'primeng/dialog';
 
 @NgModule({
   declarations: [UserRootComponent, UserDetailsComponent, UserSettingsComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    DialogModule
   ]
 })
 export class UserModule { }
