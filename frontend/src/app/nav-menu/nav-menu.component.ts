52e1ba09f21e3e161570b35b6a6fef421608145b
Tania <tatiana.gutiy@gmail.com>
2019-08-12 15:01:59 +0300

    Update current user in front

--- frontend/src/app/nav-menu/nav-menu.component.ts
+++ frontend/src/app/nav-menu/nav-menu.component.ts
@@ -11,73 +11,82 @@ import { takeUntil } from 'rxjs/operators';
 import { EventService } from '../services/event.service/event.service';
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.sass']
+    selector: 'app-nav-menu',
+    templateUrl: './nav-menu.component.html',
+    styleUrls: ['./nav-menu.component.sass']
 })
-export class NavMenuComponent implements OnInit, OnDestroy, OnChanges {
-  items: MenuItem[];
+export class NavMenuComponent implements OnInit, OnDestroy {
+    items: MenuItem[];
 
-  public dialogType = DialogType;
-  public authorizedUser: UserDTO;
-  private unsubscribe$ = new Subject<void>();
-  private userMenuBarItem=false;
+    public dialogType = DialogType;
+    public authorizedUser: UserDTO;
+    private unsubscribe$ = new Subject<void>();
+    public userMenuBarItem = false;
 
-  constructor(
-    private authDialogService: AuthDialogService,
-    private authService: AuthenticationService,
-    private eventService: EventService,
-    private userService: UserService,
-    private router: Router
-  ) { }
+    constructor(
+        private authDialogService: AuthDialogService,
+        private authService: AuthenticationService,
+        private eventService: EventService,
+        private userService: UserService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-    this.getUser();
+    ngOnInit() {
+        this.getUser();
 
-    this.eventService.userChangedEvent$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((user) => (this.authorizedUser = user ? this.userService.copyUser(user) : undefined));
-      console.log("init");
+        console.log(this.authorizedUser);
 
-      if (this.authorizedUser)
-      {
-          this.userMenuBarItem=true;
-      }
+        this.items = [
+            { label: 'Home', routerLink: [''] },
+            { label: 'Dashboard', routerLink: ['/dashboard'] },
+            { label: 'User', routerLink: ['/user'], visible: this.userMenuBarItem }
+        ];
 
-      this.items = [
-        { label: 'Home', routerLink: [''] },
-        { label: 'Dashboard', routerLink: ['/dashboard'] },
-        { label: 'User', routerLink: ['/user'],visible: this.userMenuBarItem }
-      ];
-  }
+        this.eventService.userChangedEvent$
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((user) => (this.authorizedUser = user ? this.userService.copyUser(user) : undefined));
 
-  ngOnChanges()
-  {
-      console.log("change");
-  }
-
-  public ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  public openAuthDialog(type: DialogType) {
-    this.authDialogService.openAuthDialog(type);
-  }
-  public LogOut() {
-    this.authService.logout();
-    this.authorizedUser = undefined;
-    this.router.navigate(['/']);
-  }
-
-  private getUser() {
-    if (!this.authService.areTokensExist()) {
-      return;
+        if (this.authorizedUser) {
+            console.log("loooog");
+            this.items[2].visible = true;
+        }
     }
 
-    this.authService
-      .getUser()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((user) => (this.authorizedUser = this.userService.copyUser(user)));
-  }
+    public getMenuItems()
+    {
+        if (this.authorizedUser) {
+            this.items[2].visible = true;
+        }
+        else{
+            this.items[2].visible = false;
+        }
+        return this.items;
+    }
+
+
+    public ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
+
+    public openAuthDialog(type: DialogType) {
+        this.authDialogService.openAuthDialog(type);
+    }
+    public LogOut() {
+        this.authService.logout();
+        this.authorizedUser = undefined;
+        this.router.navigate(['/']);
+    }
+
+    private getUser() {
+        if (!this.authService.areTokensExist()) {
+            return;
+        }
+
+        this.authService
+            .getUser()
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((user) => (this.authorizedUser = this.userService.copyUser(user)));
+
+    }
 }
