40646d36127fa7140dba90c46e91930a58f531d8
Yevhenii Moroziuk <kemd99@gmail.com>
2019-08-12 13:10:57 +0300

    Update project.service.ts

--- frontend/src/app/services/project.service/project.service.ts
+++ frontend/src/app/services/project.service/project.service.ts
@@ -4,6 +4,7 @@ import { ProjectCreateDTO } from '../../models/DTO/Project/projectCreateDTO';
 import { ProjectDescriptionDTO } from '../../models/DTO/Project/projectDescriptionDTO';
 import { Observable } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
+import { ProjectInfoDTO } from 'src/app/models/DTO/Project/projectInfoDTO';
 
 @Injectable({
     providedIn: 'root'
