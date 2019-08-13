33d334d61a09be4ebfd194a9e74d3307de194e54
Tania <tatiana.gutiy@gmail.com>
2019-08-13 12:58:36 +0300

    Fix

--- frontend/src/app/services/file-browser.service.ts
+++ frontend/src/app/services/file-browser.service.ts
@@ -11,16 +11,16 @@ export class FileBrowserService {
 
     constructor(private requests: HttpClientWrapperService, private convert: NodesPrepareToViewService) { }
 
-    async getProjectStructure(): Promise<ProjectStructureDTO> {
+    public async getProjectStructure(): Promise<ProjectStructureDTO> {
         const psJson = await this.requests
-            .getRequest('https://localhost:44352/ProjectStructure/5d527467b2e982133c444399')
+            .getRequest('https://localhost:44352/ProjectStructure/5d5287797404f941bcfc9e2f')
             .toPromise();
-        let ps = psJson.body as ProjectStructureDTO;
+        const ps = psJson.body as ProjectStructureDTO;
         console.log(ps);
         return ps;
 
     }
-    async getPrimeTree() {
+    public async getPrimeTree() {
         const projStruct = await this.getProjectStructure();
         return this.convert.toPrimeTree([], projStruct.nestedFiles);
 
