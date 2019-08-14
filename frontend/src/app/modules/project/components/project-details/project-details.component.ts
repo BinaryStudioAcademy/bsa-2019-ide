510016febd24bb2b4f4b5653558115bc764ab244
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2019-08-14 18:28:26 +0300

    remove obsolete imports

--- frontend/src/app/modules/project/components/project-details/project-details.component.ts
+++ frontend/src/app/modules/project/components/project-details/project-details.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { ProjectInfoDTO } from 'src/app/models/DTO/Project/projectInfoDTO';
 import { ActivatedRoute } from '@angular/router';
-import {ProjectDetailsInfoComponent} from '../project-details-info/project-details-info.component';
-import { ProjectService } from 'src/app/services/project.service/project.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-details',
