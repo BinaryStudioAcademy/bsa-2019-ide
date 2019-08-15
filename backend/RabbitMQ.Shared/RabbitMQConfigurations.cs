4e820f7a85e33d10d726705f45d9c05a03e1c1cf
Anatolii Ivanov <anatolii.0700@gmail.com>
2019-08-15 13:58:01 +0300

    Add Configure and ConfigureServices methods for each class

---
+++ backend/RabbitMQ.Shared/RabbitMQConfigurations.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Shared.Interfaces;
+using RabbitMQ.Shared.QueueServices;
+using System;
+
+namespace RabbitMQ.Shared
+{
+    public static class RabbitMQConfigurations
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IMessageQueue, MessageQueue>();
+
+            services.AddScoped<IMessageProducer, MessageProducer>();
+            services.AddScoped<IMessageProducerScope, MessageProducerScope>();
+            services.AddScoped<IMessageProducerScopeFactory, MessageProducerScopeFactory>();
+
+            services.AddScoped<IMessageConsumer, MessageConsumer>();
+            services.AddScoped<IMessageConsumerScope, MessageConsumerScope>();
+            services.AddScoped<IMessageConsumerScopeFactory, MessageConsumerScopeFactory>();
+
+            RegisterConnectionFactory(services, configuration);
+        }
+
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env) 
+        {
+
+        }
+
+        private static void RegisterConnectionFactory(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConnectionFactory>(x => new ExtendedConnectionFactory(new Uri(configuration.GetSection("RabbitMQ").Value))); //"amqp://admin:admin@localhost:5672"
+        }
+    }
+}
