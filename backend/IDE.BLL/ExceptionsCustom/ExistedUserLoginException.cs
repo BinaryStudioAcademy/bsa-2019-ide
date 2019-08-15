ba89239e0edeb739078c937375281c272688b807
Tania <tatiana.gutiy@gmail.com>
2019-08-15 16:55:37 +0300

    Fix registration with not unique email.

---
+++ backend/IDE.BLL/ExceptionsCustom/ExistedUserLoginException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDE.BLL.ExceptionsCustom
+{
+    public class ExistedUserLoginException: Exception
+    {
+        public ExistedUserLoginException():base("This E-mail is existed")
+        {
+
+        }
+    }
+}
