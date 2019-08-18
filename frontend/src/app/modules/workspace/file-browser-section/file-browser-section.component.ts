90e2c48e10aa0a32f65d4bb2fdcef979c70073fb
rshul <romander@ukr.net>
2019-08-18 15:05:15 +0300

    fix not consistent changes in filecreateDTO

--- frontend/src/app/modules/workspace/file-browser-section/file-browser-section.component.ts
+++ frontend/src/app/modules/workspace/file-browser-section/file-browser-section.component.ts
@@ -84,7 +84,8 @@ export class FileBrowserSectionComponent implements OnInit {
             name: `New File ${++this.fileCounter}`,
             content: "// Start code here:\n",
             projectId: this.projectId,
-            folder : ""
+            folder : "",
+            creatorId: 0
         }
         newFile.folder = this.getFolderName(node);
 
