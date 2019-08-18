aedda54d3c3526e8f2ccf438b49bfc8a501c0aa0
Anatolii Ivanov <anatolii.0700@gmail.com>
2019-08-18 17:59:19 +0300

    Add logging into database

--- backend/IDE.API/Controllers/ProjectController.cs
+++ backend/IDE.API/Controllers/ProjectController.cs
@@ -42,7 +42,6 @@ namespace IDE.API.Controllers
         [HttpGet("all")]
         public async Task<ActionResult<IEnumerable<ProjectDescriptionDTO>>> GetAllUserProjects()
         {
-            //Need to get userId from token
             return Ok(await _projectService.GetAllProjects(this.GetUserIdFromToken()));
         }
 
@@ -55,21 +54,18 @@ namespace IDE.API.Controllers
         [HttpGet("my")]
         public async Task<ActionResult<IEnumerable<ProjectDescriptionDTO>>> GetCreatedByUserProjects()
         {
-            //Need to get userId from token
             return Ok(await _projectService.GetUserProjects(this.GetUserIdFromToken()));
         }
 
         [HttpGet("assigned")]
         public async Task<ActionResult<IEnumerable<ProjectDescriptionDTO>>> GetAssignedToUserProjects()
         {
-            //Need to get userId from token
             return Ok(await _projectService.GetAssignedUserProjects(this.GetUserIdFromToken()));
         }
 
         [HttpGet("getFavourite")]
         public async Task<ActionResult<IEnumerable<ProjectDescriptionDTO>>> GetFavouriteUserProjects()
         {
-            //Need to get userId from token
             return Ok(await _projectService.GetFavouriteUserProjects(this.GetUserIdFromToken()));
         }
 
