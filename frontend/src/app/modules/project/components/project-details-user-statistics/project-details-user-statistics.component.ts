5a7b5e9c572f020b0569453f1cea5ed3aaa63f03
rshul <romander@ukr.net>
2019-08-27 10:02:03 +0300

    added getting statistics

--- frontend/src/app/modules/project/components/project-details-user-statistics/project-details-user-statistics.component.ts
+++ frontend/src/app/modules/project/components/project-details-user-statistics/project-details-user-statistics.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ProjectService } from './../../../../services/project.service/project.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProjectInfoDTO } from 'src/app/models/DTO/Project/projectInfoDTO';
 
 export interface ProjStat {
     userId: string,
     userName: string,
     updatedFiles: number,
-    createdFiles:  number,
-    successBuilds: number
+    createdFiles: number,
+    isAuthor: boolean
+
 }
 
 @Component({
@@ -15,9 +18,12 @@ export interface ProjStat {
 })
 export class ProjectDetailsUserStatisticsComponent implements OnInit {
 
+    @Input() project: ProjectInfoDTO;
     public usersData: ProjStat[];
+    public totalFilesNum;
     public cols: any[];
-    constructor() { }
+    constructor(private projService: ProjectService) { }
+
 
     public OpenUserDetailedPage(Id: number) {
         console.log(Id);
@@ -33,16 +39,36 @@ export class ProjectDetailsUserStatisticsComponent implements OnInit {
               this.messageService.error('Couldn`t load users statistics');
             });
         */
-       this.cols = [
-        { field: 'userName', header: 'User name' },
-        { field: 'updatedFiles', header: 'Updated files' },
-        { field: 'createdFiles', header: 'Created files' },
-        { field: 'successBuilds', header: 'Successful builds' }
-    ];
-        this.usersData = [
-            { userId : "1", userName : "username", updatedFiles : 2, createdFiles : 3, successBuilds: 4},
-            { userId : "1", userName : "username", updatedFiles : 2, createdFiles : 3, successBuilds: 2}
-        ]
+        console.log(`project id: ${this.project.id}`);
+        //let statData =  this.projService.getUsersOfProjectStatistics(this.project);
+
+        this.projService.getAuthorOfProjectStatistics(this.project).subscribe(res => {
+            const { id, name, updatedFiles, createdFiles, totalFilesNum } = res;
+            this.totalFilesNum = totalFilesNum;
+            this.usersData = [{ userId: id.toString(), userName: name, updatedFiles, createdFiles, isAuthor:true }]
+        });
+        this.projService.getUsersOfProjectStatistics(this.project).subscribe(res => {
+            const statData = res.map(x => (
+                {
+                    userId: x.id.toString(),
+                    userName: x.name,
+                    updatedFiles: x.updatedFiles,
+                    createdFiles: x.createdFiles,
+                    isAuthor:false
+                }))
+            this.usersData = [...this.usersData, ...statData];
+        });
+        this.cols = [
+            { field: 'userName', header: 'User name' },
+            {field: 'isAuthor', header: 'Project Role'},
+            { field: 'updatedFiles', header: 'Updated files' },
+            { field: 'createdFiles', header: 'Created files' },
+
+        ];
+        // this.usersData = [
+        //     { userId: "1", userName: "username", updatedFiles: 2, createdFiles: 3,  },
+        //     { userId: "1", userName: "username", updatedFiles: 2, createdFiles: 3,  }
+        // ]
         // this.userstatistics = [
         //     { id: 1, avatar: 'http://www.dsnews.ua/static/img/s/d/sddefault_771x517.jpg', name: 'Username', codelines: 10, successfulBuilds: 5, failedBuilds: 10 },
         //     { id: 2, avatar: 'http://www.dsnews.ua/static/img/s/d/sddefault_771x517.jpg', name: 'Username', codelines: 10, successfulBuilds: 5, failedBuilds: 10 },
