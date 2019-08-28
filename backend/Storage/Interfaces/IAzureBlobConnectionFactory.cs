75ab209fcc37f4fad399965496c004a325615405
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-08-28 09:22:46 +0300

    Create new storage project

---
+++ backend/Storage/Interfaces/IAzureBlobConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
+namespace Storage.Interfaces
+{
+    public interface IAzureBlobConnectionFactory
+    {
+        Task<CloudBlobContainer> GetArchiveArtifactsBlobContainer();
+        Task<CloudBlobContainer> GetDownloadedProjectZipsBlobContainer();
+        Task<CloudBlobContainer> GetBlobContainer(string containerNameKey);
+    }
+}
---
+++ backend/Storage/Interfaces/IBlobRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Storage.Interfaces
+{
+    public interface IBlobRepository
+    {
+        Task<Uri> UploadAsync(IFormFile file, int projectId, int buildId);
+        Task<Uri> UploadAsync(byte[] file, int projectId, int buildId);
+        Task<IEnumerable<Uri>> ListAsync(int projectId);
+        Task<MemoryStream> DownloadFileAsync(string fileUri);
+        Task<MemoryStream> DownloadFileAsync(string fileUri, string containerName);
+        Task DeleteAsync(string fileUri);
+        Task<Uri> UploadFileFromPathOnServer(string pathm, string directoryName);
+        Task DownloadOnDiskAsync(string blobFileName, string destinationFileName);
+    }
+}
