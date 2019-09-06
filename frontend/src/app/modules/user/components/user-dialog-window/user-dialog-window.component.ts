566bb384f440b741fa1a56d74425809cdd815780
Tania <tatiana.gutiy@gmail.com>
2019-09-06 22:00:33 +0300

    Incorrect birthday

--- frontend/src/app/modules/user/components/user-dialog-window/user-dialog-window.component.ts
+++ frontend/src/app/modules/user/components/user-dialog-window/user-dialog-window.component.ts
@@ -112,8 +112,6 @@ export class UserDialogWindowComponent implements OnInit {
 
         if(this.isUpdateInfo()) {
             this.getValuesForUpdateInfo();
-            console.log(this.userUpdateInfo);
-            debugger;
             this.userService.updateProfile(this.userUpdateInfo)
                 .subscribe(res => {
                         this.toastrService.success("Your profile info was successfully updated");
@@ -158,7 +156,6 @@ export class UserDialogWindowComponent implements OnInit {
     }
     
     private getValuesForUpdateInfo() {
-        console.log(this.userForm.get('birthday').value);
         const birthday = new Date(this.userForm.get('birthday').value);
         this.userUpdateInfo = {
             id: this.tokenService.getUserId(),
@@ -168,6 +165,7 @@ export class UserDialogWindowComponent implements OnInit {
             gitHubUrl: this.userForm.get('gitHubUrl').value,
             birthday:  birthday === null ? new Date('0001-01-01T00:00:00') : birthday
         }
+        this.userUpdateInfo.birthday.setHours(birthday.getHours()+2);
     }
 
     private getValuesForUpdatePassword(){
