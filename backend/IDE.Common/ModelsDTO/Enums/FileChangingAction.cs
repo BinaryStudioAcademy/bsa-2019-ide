e27a10e3771fc3e9ec589c695e4a72cbc90182b6
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-09-06 21:43:16 +0300

    Add FileChangingAction

---
+++ backend/IDE.Common/ModelsDTO/Enums/FileChangingAction.cs
@@ -0,0 +1,10 @@
+﻿namespace IDE.Common.ModelsDTO.Enums
+{
+    public enum FileChangingAction : int
+    {
+        Created = 0,
+        Edited = 1,
+        Renamed = 2,
+        Deleted = 3
+    }
+}
