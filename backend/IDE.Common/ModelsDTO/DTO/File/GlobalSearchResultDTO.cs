449c18666ac4cda233740c1f4dc15cf13f5de15b
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2019-09-09 11:51:31 +0300

    Redesign back and front

---
+++ backend/IDE.Common/ModelsDTO/DTO/File/GlobalSearchResultDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace IDE.Common.ModelsDTO.DTO.File
+{
+    public class GlobalSearchResultDTO
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public List<FileGlobalSearchResultDTO> FoundFiles { get; set; }
+    }
+}
